ccdf54fb3571d46bbec952a9eff607dbcb3ec6a7
Quuba <66736611+Quuba@users.noreply.github.com>
2022-03-26 18:37:53 +0100

    added things

--- src/main/java/com/example/herokudemo/HomeController.java
+++ src/main/java/com/example/herokudemo/HomeController.java
@@ -1,10 +1,7 @@
 package com.example.herokudemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,7 @@ public class HomeController {
         return questionRepository.findAll();
     }
 
+    @CrossOrigin("*")
     @GetMapping("/all/count")
     public long getQuestionCount() {
         return questionRepository.count();
