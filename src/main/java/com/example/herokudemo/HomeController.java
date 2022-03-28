c1addf6acbce5432db4c18c2eb6bef31c6e8259e
Quuba <66736611+Quuba@users.noreply.github.com>
2022-03-28 18:44:36 +0200

    ok

--- src/main/java/com/example/herokudemo/HomeController.java
+++ src/main/java/com/example/herokudemo/HomeController.java
@@ -3,10 +3,13 @@ package com.example.herokudemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class HomeController {
+
     @Autowired
     QuestionRepository questionRepository;
 
@@ -20,11 +23,25 @@ public class HomeController {
     public List<Question> findAllQuestions() {
         return questionRepository.findAll();
     }
+
     @CrossOrigin("*")
 
     @GetMapping("/all/{id}")
     public Question findQuestion(@PathVariable int id) {
         return questionRepository.findById(id);
+    }
+
+    @GetMapping("/random/{number}")
+    public List<Question> getRandom(@PathVariable int number) {
+        Random random = new Random();
+        List<Question> randomQuestions = new ArrayList<>();
+        List<Question> questions = questionRepository.findAll();
+        long questionCount = questionRepository.count();
+        for (int i = 0; i < number; i++) {
+            randomQuestions.add(questions.get(random.nextInt(0, (int)questionCount)));
+        }
+        return randomQuestions;
+
     }
 
     @CrossOrigin("*")
@@ -41,7 +58,7 @@ public class HomeController {
 
     @CrossOrigin("*")
     @PostMapping("/question")
-    public Question newQuestion(@RequestBody Question question){
+    public Question newQuestion(@RequestBody Question question) {
         return questionRepository.save(question);
     }
 }
